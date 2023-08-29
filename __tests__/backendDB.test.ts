a6972a89fc225bcf3b64f0918a2b31fdbe801ab0
gschussler <grantschussler@gmail.com>
2023-08-29 18:31:41 -0500

    test database connection

--- __tests__/backendDB.test.ts
+++ __tests__/backendDB.test.ts
@@ -3,11 +3,15 @@ import pool from '../server/Models/queryModel';
 
 dotenv.config();
 
+// console.log('supabase db name hopefully: ', process.env.SUPABASE_DB_NAME)
+
 describe('Database Connection Test', () => {
   it('should connect to the database', async () => {
     // Get the current time from the database, connection test
+    // console.log(process.env)
     try {
       const result = await pool.query('SELECT NOW()');
+      // console.log(result);
       expect(result.rows).toBeTruthy();
     } catch (error) {
       // Handle connection errors
