d1adcc1bb4f9031d0a897872efd67e871dcf9ed3
Ronagens <TristanKeester@Gmail.com>
2023-08-29 12:19:56 -0500

    Working on fetch functionality in presetner creation

--- client/containers/Presenter.tsx
+++ client/containers/Presenter.tsx
@@ -1,31 +1,46 @@
 import React, { useState } from 'react';
 
+interface FetchBody {
+  
+}
+
 function Presenter() {
   // Will have pollId gotten from backend and passed in?
   const pollId = 1;
-  const [answers, setAnswers] = useState<React.JSX.Element[]>([]);
-  const [createAnswerPopup, setCreateAnswerPopup] = useState(false);
-
-  function showAnswerPopup() {
-    setCreateAnswerPopup(!createAnswerPopup)
-  }
+  const [answers, setAnswers] = useState<string[]>([]);
   
+  function createPoll() {
+    const fetchBody = {
+      question: (document.getElementById('pollQuestion') as HTMLFormElement).value,
+      answers 
+    }
+
+    fetch(`/api/poll/startPoll${pollId}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(fetchBody)
+    })
+    .catch((err) => console.log(err));
+  }
+
   function createNewAnswer(e: React.FormEvent): void {
     e.preventDefault();
 
     const answerInput = (document.getElementById('newAnswerInput') as HTMLInputElement);
     const answersCopy = answers.slice();
-    const answersLen = answers.length + 1;
 
-    answersCopy.push(
-    <div key={`createPollAnswer${answersLen}`}>
-      <p>{answersLen}: {answerInput.value}</p>
-    </div>
-    )
+    answersCopy.push(answerInput.value)
 
+    answerInput.value = null;
     setAnswers(answersCopy);
-    showAnswerPopup();
   }
+
+  const answersElements: React.JSX.Element[] = []
+  answers.forEach((el, i) => {
+    answersElements.push(<p key={`answerElementKey${el}`}>{i + 1}: {el}</p>)
+  })
 
   return (
     <div>
@@ -35,17 +50,16 @@ function Presenter() {
         <input type='text' id='pollQuestion' name='pollQuestion' placeholder='Question' />
       </label>
       <br />
-      <button type='button' onClick={showAnswerPopup}>Create New Answer</button>
-      {
-      createAnswerPopup ? 
-        <form onSubmit={(e) => createNewAnswer(e)}>
+      <form onSubmit={(e) => createNewAnswer(e)}>
+        <label htmlFor='newAnswerInput'>
+          New Answer:
           <input type='text' id='newAnswerInput' name='newAnswerInput' placeholder='Answer'/> 
-          <button type='submit'>Create</button>
-        </form>
-        : null
-      }
+        </label>
+        <button type='submit'>Create</button>
+      </form>
+      <button type='button' onClick={createPoll}>Create Poll</button>
       <p>Answers:</p>
-      {answers}
+      {answersElements}
     </div>
   );
 }
