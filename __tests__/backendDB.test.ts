5824fa5691798e477289aed77df6e6ec165cb74f
gschussler <grantschussler@gmail.com>
2023-08-29 16:34:09 -0500

    simple test added, possibly config error

--- __tests__/backendDB.test.ts
+++ __tests__/backendDB.test.ts
@@ -1,35 +1,17 @@
 import * as dotenv from 'dotenv';
-import { Pool, QueryResult } from 'pg';
+import pool from '../server/Models/queryModel';
 
 dotenv.config();
-
-const DB_PORT = parseInt(process.env.SUPABASE_PORT, 10)
-
-const poolConfig = {
-  user: process.env.SUPABASE_USER,
-  database: process.env.SUPABASE_DB_NAME,
-  password: process.env.SUPABASE_PASSWORD,
-  port: DB_PORT,
-  host: process.env.SUPABASE_HOST
-}
-
-// console.log(poolConfig);
-
-const pool = new Pool(poolConfig);
 
 describe('Database Connection Test', () => {
   it('should connect to the database', async () => {
     // Get the current time from the database, connection test
-    const query = 'SELECT NOW()';
-    let result: QueryResult;
-
     try {
-      result = await pool.query(query);
+      const result = await pool.query('SELECT NOW()');
+      expect(result.rows).toBeTruthy();
     } catch (error) {
       // Handle connection errors
       throw new Error(`Error connecting to the database: ${error}`);
     }
-
-    expect(result.rows.length).toBeGreaterThan(0);
   });
 });
