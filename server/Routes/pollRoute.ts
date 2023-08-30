917c4bf65ee52df5cacd308a81c12aef965ba9ef
peterzepf <ptrgzpf@gmail.com>
2023-08-30 15:56:03 -0400

    set up to use socket io

--- server/Routes/pollRoute.ts
+++ server/Routes/pollRoute.ts
@@ -2,86 +2,92 @@
  * File handles responding to requests levied at the /poll/* endpoints
  */
 import express from 'express';
-import PollController from '../Controllers/PollController';
+import { Server } from 'socket.io';
+import createPollController from '../Controllers/PollController';
 import UserController from '../Controllers/UserController';
 import { Question } from '../../types/types';
 
-const router = express.Router();
+const pollRouteFactory = (io: Server) => {
+  const router = express.Router();
+  const PollController = createPollController(io);
 
-/**
- * Endpoint for creating empty poll; expect another request from frontend
- * with questions and other details before starting poll
- * Responds with new room code
- */
-router.get(
-  '/createPoll',
-  UserController.createPresenter,
-  PollController.createPoll,
-  (req, res) => {
-    const { poll_id } = res.locals as { poll_id: number };
-    res.status(200).json({ roomCode: poll_id });
-  },
-);
+  /**
+   * Endpoint for creating empty poll; expect another request from frontend
+   * with questions and other details before starting poll
+   * Responds with new room code
+   */
+  router.get(
+    '/createPoll',
+    UserController.createPresenter,
+    PollController.createPoll,
+    (req, res) => {
+      const { poll_id } = res.locals as { poll_id: number };
+      res.status(200).json({ roomCode: poll_id });
+    },
+  );
 
-/**
- * Endpoint for populating poll with questions and starting lifetime countdown
- * on poll
- * Request body must contain information about questions and may contain a duration
- *
- * May also need to start websocket connection here?
- */
-router.patch(
-  '/startPoll/:roomCode',
-  PollController.verifyRoomCode,
-  PollController.populateQuestions,
-  PollController.setLifetime, // TODO this is stretch goal
-  PollController.startPoll,
-  (req, res) => {
-    res.sendStatus(200);
-  },
-);
+  /**
+   * Endpoint for populating poll with questions and starting lifetime countdown
+   * on poll
+   * Request body must contain information about questions and may contain a duration
+   *
+   * May also need to start websocket connection here?
+   */
+  router.patch(
+    '/startPoll/:roomCode',
+    PollController.verifyRoomCode,
+    PollController.populateQuestions,
+    PollController.setLifetime, // TODO this is stretch goal
+    PollController.startPoll,
+    (req, res) => {
+      res.sendStatus(200);
+    },
+  );
 
-/**
- * Endpoint for stopping poll
- * This will be where we add functionality to save poll results to a presenter
- */
-router.patch(
-  '/endPoll/:roomCode',
-  PollController.verifyRoomCode,
-  PollController.stopPoll,
-  (req, res) => {
-    res.sendStatus(200);
-  },
-);
+  /**
+   * Endpoint for stopping poll
+   * This will be where we add functionality to save poll results to a presenter
+   */
+  router.patch(
+    '/endPoll/:roomCode',
+    PollController.verifyRoomCode,
+    PollController.stopPoll,
+    (req, res) => {
+      res.sendStatus(200);
+    },
+  );
 
-/**
- * Audience member pulling questions for them to answer after reaching endpoint at
- * a particular roomcode
- */
-router.get(
-  '/questionsInPoll/:roomCode',
-  PollController.verifyRoomCode,
-  PollController.getQuestionsInPoll,
-  (req, res) => {
-    const { questions } = res.locals as { questions: Question[] };
-    res.status(200).json({ questions });
-  },
-);
+  /**
+   * Audience member pulling questions for them to answer after reaching endpoint at
+   * a particular roomcode
+   */
+  router.get(
+    '/questionsInPoll/:roomCode',
+    PollController.verifyRoomCode,
+    PollController.getQuestionsInPoll,
+    (req, res) => {
+      const { questions } = res.locals as { questions: Question[] };
+      res.status(200).json({ questions });
+    },
+  );
 
-/**
- * Audience member submitting all responses to poll questions
- * Request body needs to contain question:answer pairs
- */
-router.post(
-  '/newAnswers/:roomCode',
-  PollController.verifyRoomCode,
-  PollController.checkOpen,
-  PollController.recordResponses,
-  (req, res) => {
-    res.sendStatus(200);
-  },
-);
+  /**
+   * Audience member submitting all responses to poll questions
+   * Request body needs to contain question:answer pairs
+   */
+  router.post(
+    '/newAnswers/:roomCode',
+    PollController.verifyRoomCode,
+    PollController.checkOpen,
+    PollController.recordResponses,
+    (req, res) => {
+      res.sendStatus(200);
+    },
+  );
+
+  return router;
+};
 
 // See /test routes for placeholder getPollAnswerData before websocket implementation
 
-export default router;
+export default pollRouteFactory;
